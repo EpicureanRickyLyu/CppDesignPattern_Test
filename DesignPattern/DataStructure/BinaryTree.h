952a2e9bb5c313d3b788a8c2847a8d98f7ac10a3
RickyLyu <727862579@qq.com>
2022-10-26 22:34:32 +0100

    binary tree

--- DesignPattern/DataStructure/BinaryTree.h
+++ DesignPattern/DataStructure/BinaryTree.h
@@ -1,2 +1,36 @@
 #pragma once
 #include <iostream>
+using namespace std;
+struct node
+{
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+// Traverse Preorder
+void traversePreOrder(struct node* temp) {
+    if (temp != NULL) {
+        cout << " " << temp->data;
+        traversePreOrder(temp->left);
+        traversePreOrder(temp->right);
+    }
+}
+
+// Traverse Inorder
+void traverseInOrder(struct node* temp) {
+    if (temp != NULL) {
+        traverseInOrder(temp->left);
+        cout << " " << temp->data;
+        traverseInOrder(temp->right);
+    }
+}
+
+// Traverse Postorder
+void traversePostOrder(struct node* temp) {
+    if (temp != NULL) {
+        traversePostOrder(temp->left);
+        traversePostOrder(temp->right);
+        cout << " " << temp->data;
+    }
+}
